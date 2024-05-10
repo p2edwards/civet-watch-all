9d17691a7cf590fc87c272820dc181139b72c0dd
Philip Edwards <phil@edwards.io>
2024-05-10 17:01:46 -0400

    Make it work with newer Civet versions

--- index.js
+++ index.js
@@ -25,12 +25,19 @@ const CIVET_COMPILE_OPTIONS = {
     // js: true,
     // inlineMap: true,
 }
-const OUTPUT_EXTENSION = '.ts' // See footnote [1]
+// const PREPEND_DIRECTIVES =
+// `"civet react"
+// `
+// ^ See footnote [1]
+const OUTPUT_EXTENSION = '.tsx' // See footnote [2]
 
 async function compileFile(path) {
     const outputPath = path.replace(/(\.civet)$/, OUTPUT_EXTENSION)
     const civetCode = await fs.readFile(path, 'utf8')
-    const compiled = compile(civetCode, CIVET_COMPILE_OPTIONS)
+    const compiled = await compile(
+      (PREPEND_DIRECTIVES || '') + civetCode,
+      CIVET_COMPILE_OPTIONS
+    )
     console.log(`${time()} - ⏩`, outputPath)
     await fs.writeFile(outputPath, compiled, 'utf8')
     return outputPath
@@ -48,5 +55,11 @@ async function fileExists(path) { return !!(await fs.stat(path).catch(e => false
 function time() { return new Date().toLocaleTimeString() }
 
 // Footnotes
-// - [1]: To support both .ts and .tsx, you could use OUTPUT_EXTENSION = ''
-//        with filenames like example1.ts.civet, example2.tsx.civet
+// - [1]: To get the text editor LSP to read directives, repeat them
+//        in a civetconfig.json: `{ "parseOptions": { "react": true } }`
+//        (It'd be cool if I read the config here, but I didn't find a simple or built-in
+//         way to do that yet, so, prepending it is.)
+// 
+// - [2]: You'd think you can use OUTPUT_EXTENSION = '' with filenames like
+//        example1.ts.civet, and example2.tsx.civet. But, files named this way
+//        break the Civet/TS LSP integration somehow.
